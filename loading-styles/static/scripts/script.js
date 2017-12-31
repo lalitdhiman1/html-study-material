4393ce92b6e942e2f942d69c3a5eb7ff334bf44c
Lalit Dhiman <lalitdhiman1@gmail.com>
2017-12-31 12:32:57 +0530

    js changes

--- loading-styles/static/scripts/script.js
+++ loading-styles/static/scripts/script.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
   function loadingSpan(){
-   $(".loading span").each(function(){
-    if($(this).hasClass("active")){
-     $(this).removeClass("active");
-     $(this).next().addClass("active");
-    }
-   })
+   // $(".loading span").each(function(){
+   //  if($(this).hasClass("active")){
+   //   $(this).removeClass("active");
+   //   $(this).next().addClass("active");
+   //  }
+   // })
+   let countSpan = $(".loading span").length;
+   for(var i = 0; i < countSpan; i++){
+    $(".loading span:eq("+i+")").html(i)
+   }
   }
   setInterval(loadingSpan, 3000)
 })
